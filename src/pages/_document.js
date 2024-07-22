2c89b658fcf027bf8b02811bab2f1ce4df1ba60d
Stoica Robert <stoicarobert38@yahoo.com>
2024-07-22 23:38:20 +0300

    Solved crossOrigin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
        <Head>
        <meta charset="UTF-8" />
 <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 <link
     href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&display=swap"
     rel="stylesheet"
